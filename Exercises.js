9488aa6bd43a0b954c171b6384fb4e14065e68a6
Luke Parker <luke.parker9@tafensw.edu.au>
2020-05-20 15:43:27 +1000

    Added Daily Challenge

--- Exercises.js
+++ Exercises.js
@@ -24,8 +24,8 @@ function multiplyNumbers(num1, num2) {
 
 const multiplyNumbersArrow = (num1, num2) => num1 * num2;
 
-console.log(multiplyNumbers(12,4));
-console.log(multiplyNumbersArrow(12,4));
+console.log(multiplyNumbers(12, 4));
+console.log(multiplyNumbersArrow(12, 4));
 
 // 2 Convert this function to an arrow function
 function personOver40(person) {
@@ -96,4 +96,20 @@ console.log(getPersonTitleArrow(person));
 
 
 // Throwing and handling exceptions
+// 1 Create a function called divide that takes 2 numbers as parameters. The function should divide the first number by the second number and return the result. If the second number is a 0, the function should throw an Error object.
+function divide(num1, num2) {
+    if (num2 == 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return num1 / num2;
+}
+
+
+// 2 Call the above function inside a try/catch block. You should log the exception message and then continue with the program.
+
+try {
+    console.log(divide(10, 0));
+} catch (e) {
+    console.log(e.message);
+}
 
